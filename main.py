bebb817ff392414a714bad2e3be279037c8b63a5
JRcepeda <josercepeda01@gmail.com>
2024-08-08 00:39:03 +0000

    Update requirements-GCP

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask import jsonify
-
+import os
 app=Flask(__name__)
+
 @app.route('/')
 def hello():
     """Return a frinedly HTTP greeting."""
@@ -15,4 +16,5 @@ def echo(name):
     return jsonify(val)
 
 if __name__=='__main__':
-    app.run(host='127.0.0.1',port=8080,debug=True)
+    #app.run(host='127.0.0.1',port=8080,debug=True)
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT',8080)))
